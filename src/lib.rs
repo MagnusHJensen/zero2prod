f79ebebb64ee243340f692b2dcee0522cbc6d886
MagnusHJensen <magnus.holm.jensen@lego.dk>
2023-03-19 20:42:47 +0100

    better package split

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,3 @@
-use std::io::Error;
-use std::net::TcpListener;
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
-use actix_web::dev::Server;
-
-// We need to mark `run` as public.
-// It is no longer a binary entrypoint, therefore we can mark it as async
-// without having to use any proc-macro incantation.
-pub fn run(listener: TcpListener) -> Result<Server, Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-        })
-        .listen(listener)?
-        .run();
-    Ok(server)
-}
-
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use zero2prod::startup::run;
 
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
@@ -24,6 +25,54 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let form = [("name", "le guin"), ("email", "ursula_le_guin@gmail.com")];
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .form(&form)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16(), "{}", response.text().await.unwrap());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ([("name", "le guin")], "missing the email"),
+        ([("email", "ursula_le_guin@gmail.com")], "missing the name"),
+        ([("", "")], "missing both name and email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .form(&invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
+
 // No .await call, therefore no need for `spawn_app` to be async now.
 // We are also running tests, so it is not worth it to propagate errors:
 // if we fail to perform the required setup we can just panic and crash
@@ -32,7 +81,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port.");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let.
